7e28921a93157383b15f2a23c34fc54240c22af7
LeninJimenezTorres <jlenin25@hotmail.com>
2022-03-08 12:17:56 -0500

    Updates

---
+++ component---src-pages-index-js-e8d8b5b6ff2fb5b41b51.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[678],{558:function(e,t,a){a.r(t),a.d(t,{default:function(){return n}});var l=a(294);function n(){return l.createElement("div",null,"This page is developed with React + Gatsby and deployed in GitHub pages!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-e8d8b5b6ff2fb5b41b51.js.map
